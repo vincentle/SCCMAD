9ef4bedbc95ac1ca77e9a98ea2256d560cfd5f26
vincentle <vince.le95@gmail.com>
2018-10-23 13:48:43 -0400

    Updated window sizing, improved search algo, added last logon header

--- SCCMAD beta v0.9.ps1
+++ SCCMAD beta v0.9.ps1
@@ -8,32 +8,31 @@ $inputXML = @"
         xmlns:local="clr-namespace:SCCMAD"
         ResizeMode="NoResize"
         mc:Ignorable="d"
-        Title="SCCMAD" Height="353.176" Width="600">
-    <Grid Margin="0,0,0,-29" HorizontalAlignment="Right" Width="594" Height="353" VerticalAlignment="Bottom">
+        Title="SCCMAD" Height="586.176" Width="600">
+    <Grid Margin="0,0,0,-29" HorizontalAlignment="Right" Width="594" Height="351" VerticalAlignment="Bottom">
         <Grid.ColumnDefinitions>
             <ColumnDefinition/>
             <ColumnDefinition/>
         </Grid.ColumnDefinitions>
-        <Image Height="90" Margin="198,0" VerticalAlignment="Top" Source="https://image.flaticon.com/icons/svg/25/25231.svg" Grid.ColumnSpan="2"/>
+        <Image Height="102" Margin="200,-231,196,0" VerticalAlignment="Top" Source="https://image.flaticon.com/icons/svg/25/25231.svg" Grid.ColumnSpan="2"/>
 
-        <TextBox x:Name="Input" Margin="40,95,125,235" TextWrapping="Wrap" Grid.ColumnSpan="2" RenderTransformOrigin="0.501,0.522"/>
-        <Label x:Name="secondTitle" Content="Tool used for removal of devices from SCCM and/or AD" Margin="127,64,126,0" VerticalAlignment="Top" Grid.ColumnSpan="2" FontWeight="Bold"/>
-        <ListView x:Name="Grid" Margin="40,123,40,107" Grid.ColumnSpan="2">
+        <TextBox x:Name="Input" Margin="40,-124,125,454" TextWrapping="Wrap" Grid.ColumnSpan="2" RenderTransformOrigin="0.501,0.522"/>
+        <Label x:Name="secondTitle" Content="Tool used for removal of devices from SCCM and/or AD" Margin="128,-155,125,0" VerticalAlignment="Top" Grid.ColumnSpan="2" FontWeight="Bold"/>
+        <ListView x:Name="Grid" Margin="40,-98,40,107" Grid.ColumnSpan="2">
             <ListView.View>
                 <GridView>
                     <GridViewColumn Header="Name" DisplayMemberBinding ="{Binding Name}" Width="120"/>
-                    <GridViewColumn Header="Distinguished Name" DisplayMemberBinding ="{Binding DistinguishedName}" Width="120"/>
-                    <GridViewColumn Header="IP Address" DisplayMemberBinding ="{Binding IPv4Address}" Width="120"/>
+                    <GridViewColumn Header="Distinguished Name" DisplayMemberBinding ="{Binding DistinguishedName}" Width="215"/>
+                    <GridViewColumn Header="Last Logon" DisplayMemberBinding ="{Binding LastLogonDate}" Width="150"/>
                 </GridView>
             </ListView.View>
         </ListView>
-        <Button x:Name="OK" Content="GO" Grid.Column="1" Margin="180,95,40,235" IsDefault="True"/>
+        <Button x:Name="OK" Content="GO" Grid.Column="1" Margin="180,-124,40,454" IsDefault="True"/>
         <Button x:Name="SCCM" Content="SCCM" HorizontalAlignment="Left" Margin="127,277,0,56" Width="75"/>
         <Button x:Name="Both" Grid.ColumnSpan="2" Content="Both" Margin="250,277,252,56"/>
         <Button x:Name="AD" Content="AD" Grid.Column="1" Margin="87,277,127,56"/>
         <Label Content="Remove from:" Margin="250,251,250,76" Grid.ColumnSpan="2" FontWeight="Bold"/>
         <TextBlock x:Name="textBlock" HorizontalAlignment="Left" Margin="10,302,0,0" TextWrapping="Wrap" VerticalAlignment="Top" Grid.ColumnSpan="2" Width="567" Height="20"/>
-
     </Grid>
 </Window>
 "@ 
@@ -81,14 +80,19 @@ import-module "C:\Program Files (x86)\Microsoft Configuration Manager\AdminConso
 #===========================================================================
 
 Function Get-ADDevice{
-    param($computername =$env:COMPUTERNAME) 
-    Get-ADComputer -Filter "name -like '$computername*'" -SearchBase "OU=PLACE,DC=OU,DC=HERE,DC=XXXX" -Properties Name, DistinguishedName, IPv4Address | Select-Object Name, DistinguishedName, IPv4Address
+    param($computername =$env:COMPUTERNAME)
+    Get-ADComputer -Filter "name -like '$computername*'" -SearchBase "OU=PLACE,DC=OU,DC=HERE,DC=XXXX" -Properties Name, DistinguishedName, LastLogonDate | Select-Object Name, DistinguishedName, LastLogonDate
+}
+
+Function Get-ADVariable{
+    param($computername =$env:COMPUTERNAME)
+    Get-ADComputer -Filter "name -like '$computername*'" -SearchBase "OU=PLACE,DC=OU,DC=HERE,DC=XXXX" -Properties Name | Select-Object Name
 }
 
 Function Get-SCCMDevice{
     param($computername =$env:COMPUTERNAME) 
     cd PLACE SCCM DIRECTORY/SITE HERE:
-    Get-CMDevice -name $computername* | Select-Object Name
+    Get-CMDevice -name $computername* | Select-Object name
 }
 
 Function Delete-ADDevice{
@@ -100,8 +104,8 @@ Function Delete-ADDevice{
     $Result = [System.Windows.MessageBox]::Show($MessageBody,$MessageTitle,$ButtonType,$MessageIcon)
     Write-Host "Your choice is $Result"
         if($Result -eq "Yes"){
-            $WPFtextBlock.Text = "Last deleted: '$data'. Good job!"
             Remove-ADComputer -Identity $data -Confirm:$false 
+            $WPFtextBlock.Text = "Last deleted: '$data'. Good job!"
         }
         else{
             $WPFtextBlock.Text = "Deletion CANCELLED on: '$data'. Good job!"
@@ -115,10 +119,10 @@ Function Delete-CMDevice{
     $MessageBody = "Are you sure you want to delete '$data' from SCCM?"
     $MessageTitle = "Confirm Deletion of Each Object"
     $Result = [System.Windows.MessageBox]::Show($MessageBody,$MessageTitle,$ButtonType,$MessageIcon)
-    Write-Output "Your choice is $Result"
+    Write-Host "Your choice is $Result"
         if($Result -eq "Yes"){
+            Remove-CMDevice -DeviceName $data
             $WPFtextBlock.Text = "Last deleted: '$data'. Good job!"
-            Remove-CMDevice -DeviceName $data -Force
         }
         else{
             $WPFtextBlock.Text = "Deletion CANCELLED on: '$data'. Good job!"
@@ -136,22 +140,50 @@ Function Create-ErrorMessage{
 #Populates ListView with computer info after clicking OK button
 $WPFOK.Add_Click({
     $WPFGrid.Items.Clear()
+    $WPFtextBlock.Text = ""
     try{
         if ([string]::IsNullOrWhiteSpace($WPFInput.Text)){
             $WPFtextBlock.Text = "ERROR: EMPTY STRING/WHITESPACE, TYPE SOMETHING CMON"
         }
         else{
-            $ADDevice = Get-ADDevice -computername $WPFInput.Text | % {$WPFGrid.AddChild($_)}
-            $SCCMDevice = Get-SCCMDevice -computername $WPFInput.Text | % {$WPFGrid.AddChild($_)}
-            if ($ADDevice -eq $SCCMDevice){
-                Write-Host "Hello"
-                Write-Host $ADDevice
-                Write-Host $SCCMDevice
+            #Takes input and turns device names into array for parsing
+            $adv = Get-ADVariable -computername $WPFInput.Text
+            $scv = Get-SCCMDevice -computername $WPFInput.Text
+            write-host $adv
+            write-host $scv
+            $ADarr = $adv.name -split ' '
+            $SCarr = $scv.name -split ' '
+            $SCCMADarr = $ADarr + $SCarr
+            $uniq = $SCCMADarr | select -uniq
+            write-host $uniq
+            write-host $SCCMADarr[0]
+            $meme = "@{name=||SCCM DEVICES BELOW||}"
+            if($adv){
+                foreach($name in $ADarr){
+                    Get-ADDevice -computername $name | % {$WPFGrid.AddChild($_)}
+                }
+                if($scv){
+                    $WPFGrid.AddChild($meme)
+                    foreach($name in $SCarr){
+                        Get-SCCMDevice -computername $name | % {$WPFGrid.AddChild($_)}
+                    }
+                    $WPFtextBlock.Text = "NOTICE: Devices found in both databases. Be sure to match names before deletion."
+                }
+                else{
+                    $WPFtextBlock.Text = "NOTICE: Device found in AD"
+                    write-host 'variable is null' -ErrorAction Stop
+                }
             }
-            #foreach (ADDevice in string){
-                #compare to SCCMDevice string
-                #Print not equals
-            #}
+            elseif($scv){
+                foreach($name in $SCarr){
+                    Get-SCCMDevice -computername $name | % {$WPFGrid.AddChild($_)}
+                    $WPFtextBlock.Text = "NOTICE: Device found in SCCM"
+                }
+            }
+            else{
+                $WPFtextBlock.Text = "ERROR: Device not found"
+                write-host 'variable is null' -ErrorAction Stop
+            }
         }
     }
     catch{
